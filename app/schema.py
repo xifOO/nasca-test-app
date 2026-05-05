836861404f92467a51f1fab7c85523558581cedc
xifOO <wedyi28111@gmail.com>
2026-05-06 01:52:07 +0300

    fix: исправить str на EmailStr

--- app/schema.py
+++ app/schema.py
@@ -1,16 +1,16 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class UserCreate(BaseModel):
     name: str
-    email: str
+    email: EmailStr
     age: Optional[int] = None
 
 
 class User(BaseModel):
     id: str
     name: str
-    email: str
+    email: EmailStr
     age: Optional[int] = None
